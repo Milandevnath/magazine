a2375e16e3ad02c472bd34e802ca5a50f8f47446
Sirus <sirusdas@gmail.com>
2016-07-04 16:16:05 +0530

    Wordpress version Created Database name: Magazine-Wordpress
    SQL Username: root
    Pass: root

---
+++ wordpress/wp-content/themes/amigo/footer.php
@@ -0,0 +1,13 @@
+<footer id="colophon" class="rsrc-footer" role="contentinfo">
+   <div class="row rsrc-author-credits">
+     <p class="text-center"><?php printf( __( 'Created by %1$s.', 'amigo' ), '<a href="'.esc_url("http://themes4wp.com").'">Themes4WP</a>' ); ?></p>
+   </div>       
+</footer> 
+<p id="back-top">
+  <a href="#top"><span></span></a>
+</p>
+<!-- end main container -->
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
